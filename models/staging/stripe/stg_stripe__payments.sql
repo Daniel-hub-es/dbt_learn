0262bb6ffeb3fd0ae396b5b76ab78434a1037e6d
Daniel Valbuena <valbuena1v2b@gmail.com>
2025-06-08 17:01:21 +0000

    adapt sources and models to BigQuery format and adapter

--- models/staging/stripe/stg_stripe__payments.sql
+++ models/staging/stripe/stg_stripe__payments.sql
@@ -7,13 +7,13 @@ with
 
     payments as(
         select 
-            "ID" as payment_id,
-            "ORDERID" as order_id,
-            "PAYMENTMETHOD" as payment_method,
-            "STATUS" as payment_status,
-            {{ cents_to_dollars('"AMOUNT"') }} as amount,
-            round("AMOUNT"/10.0, 2) as payment_amount,
-            "CREATED" as created_at
+            ID as payment_id,
+            ORDERID as order_id,
+            PAYMENTMETHOD as payment_method,
+            STATUS as payment_status,
+            {{ cents_to_dollars('amount') }} as amount,
+            round(amount/10.0, 2) as payment_amount,
+            CREATED as created_at
         from source
     )
 
